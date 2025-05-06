cc78ff5ec1ee5031c7488f77494f8a23e70cebbb
Sérgio Deivis <gtisergio@gmail.com>
2025-05-06 13:00:10 -0300

    1.0.15

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1371984702"
+    serviceWorkerVersion: "1916544374"
   }
 });
